cbb220312fe2c6767a52073e5f2087a4f349a14e
mothworldwideinfo <mothworldwideinfo@gmail.com>
2026-08-29 15:48:58 -0400

    Add service worker

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'moth-app-v1';
+const PRECACHE = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(PRECACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((hit) => {
+      const net = fetch(event.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(event.request, copy)).catch(() => {});
+          return res;
+        })
+        .catch(() => hit);
+      return hit || net;
+    })
+  );
+});
